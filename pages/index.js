49839254db0f4fa405a5e7141de6f4f6bdf1816a
lunaroyster <oneinaquadrillion@live.com>
2018-04-04 13:19:14 +0000

    indexes

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export { default as TheIssue } from './TheIssue';
+export { default as HowLeaksHappen } from './HowLeaksHappen';
+export { default as FixingLeaks } from './FixingLeaks';
+export { default as Unleakable } from './Unleakable';
+export { default as Viability } from './Viability';
+export { default as Details } from './Details';
+export { default as Fairness } from './Fairness';
